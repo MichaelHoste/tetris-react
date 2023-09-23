5fba4681fbe62eaa85e538f1191aaac8d4474a00
Michaël Hoste <michael.hoste@gmail.com>
2023-09-23 15:45:23 +0200

    Draft of state/css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './css/styles.css';
+
+class Tetris extends React.PureComponent {
+  HEIGHT = 20;
+  WIDTH  = 10;
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      grid: this.initializeGrid()
+    }
+  }
+
+  render() { return }
+
+  initializeGrid() {
+    return Array.from({ length: this.HEIGHT }).map(() =>
+      Array.from({ length: this.WIDTH }).fill('')
+    )
+  }
+
+  render() {
+    return (
+      <div className="tetris">
+        { this.renderGrid() }
+      </div>
+    )
+  }
+
+  renderGrid() {
+    return this.state.grid.map((row, i) => this.renderRow(row, i))
+  }
+
+  renderRow(row, i) {
+    return (
+      <div className="row" key={`row_${i}`}>
+        { row.map((cell, i) => this.renderCell(cell, i)) }
+      </div>
+    )
+  }
+
+  renderCell(cell, i) {
+    return (
+      <div className="cell" key={`cell_${i}`}>
+        &nbsp;
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Tetris;
