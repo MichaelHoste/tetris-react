9ffd1298c3acdc3ce7fd1cd4d9d00ff64e380b1f
Michaël Hoste <michael.hoste@gmail.com>
2023-10-23 18:37:25 +0200

    Extract next pieces into subcomponent

--- src/Tetris.js
+++ src/Tetris.js
@@ -1,4 +1,7 @@
-import React    from 'react';
+import React from 'react';
+
+import NextPieces from './components/NextPieces';
+
 import Pieces   from './utils/Pieces';
 import shuffle  from './utils/FisherYatesShuffle'
 import urlParam from './utils/UrlParam'
@@ -457,34 +460,6 @@ class Tetris extends React.PureComponent {
     return Pieces.COLORS[letter]
   }
 
-  next3Pieces() {
-    // Take 3 next pieces from first bag (starting from last)
-    let pieces = this.state.bags[0].slice(-3).reverse()
-
-    // Merge the remaining pieces from next bag, still starting from last
-    if(pieces.length < 3) {
-      pieces = pieces.concat(
-        this.state.bags[1].slice(-3 + pieces.length).reverse()
-      )
-    }
-
-    // Need to call the function to generate piece
-    pieces = pieces.map((piece) => piece())
-
-    // Remove empty rows for better alignment
-    pieces.forEach((piece) => {
-      while(piece[0].every((cell) => cell === ' ')) {
-        piece.shift() // remove from start (only 'i')
-      }
-
-      while(piece[piece.length - 1].every((cell) => cell === ' ')) {
-        piece.pop() // remove from end (fast!)
-      }
-    })
-
-    return pieces
-  }
-
   render() {
     return (
       <div className="tetris-container">
@@ -492,14 +467,11 @@ class Tetris extends React.PureComponent {
           <div className="tetris-grid">
             { this.renderGrid() }
           </div>
-          <div className="next-piece">
-            <h2>Next</h2>
-            { this.renderNextPieces() }
-          </div>
+          <NextPieces bags={this.state.bags} />
         </div>
-        { this.renderScore() }
+        { this.renderScore()    }
         { this.renderGameOver() }
-        { this.renderPause() }
+        { this.renderPause()    }
       </div>
     )
   }
@@ -568,42 +540,6 @@ class Tetris extends React.PureComponent {
         </div>
       )
     }
-  }
-
-  renderNextPieces() {
-    return this.next3Pieces().map((piece, i) => {
-      return (
-        <div className="piece" key={i}>
-          { piece.map((row, i) => this.renderPieceRow(row, i)) }
-        </div>
-      )
-    })
-  }
-
-  renderPieceRow(row, i) {
-    const key       = `piece-row-${i}`
-    const className = `piece-row ${key}`
-
-    return (
-      <div className={className} key={key}>
-        { row.map((cell, j) => this.renderPieceCell(cell, i, j)) }
-      </div>
-    )
-  }
-
-  renderPieceCell(cell, i, j) {
-    const key       = `piece-cell-${i}-${j}`
-    let className = `piece-cell ${key}`
-
-    if(cell !== ' ') {
-      className = `${className} piece-cell-full`
-    }
-
-    return (
-      <div className={className} key={key} style={{ backgroundColor: Pieces.COLORS[cell] }}>
-        &nbsp;
-      </div>
-    )
   }
 }
 
---
+++ src/components/NextPieces.js
@@ -0,0 +1,87 @@
+import React from 'react';
+
+import Pieces from '../utils/Pieces';
+
+class NextPieces extends React.PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+    }
+  }
+
+  next3Pieces() {
+    // Take 3 next pieces from first bag (starting from last)
+    let pieces = this.props.bags[0].slice(-3).reverse()
+
+    // Merge the remaining pieces from next bag, still starting from last
+    if(pieces.length < 3) {
+      pieces = pieces.concat(
+        this.props.bags[1].slice(-3 + pieces.length).reverse()
+      )
+    }
+
+    // Need to call the function to generate piece
+    pieces = pieces.map((piece) => piece())
+
+    // Remove empty rows for better alignment
+    pieces.forEach((piece) => {
+      while(piece[0].every((cell) => cell === ' ')) {
+        piece.shift() // remove from start (only 'i')
+      }
+
+      while(piece[piece.length - 1].every((cell) => cell === ' ')) {
+        piece.pop() // remove from end (fast!)
+      }
+    })
+
+    return pieces
+  }
+
+  render() {
+    return(
+      <div className="next-piece">
+        <h2>Next</h2>
+        { this.renderNextPieces() }
+      </div>
+    )
+  }
+
+  renderNextPieces() {
+    return this.next3Pieces().map((piece, i) => {
+      return (
+        <div className="piece" key={i}>
+          { piece.map((row, i) => this.renderPieceRow(row, i)) }
+        </div>
+      )
+    })
+  }
+
+  renderPieceRow(row, i) {
+    const key       = `piece-row-${i}`
+    const className = `piece-row ${key}`
+
+    return (
+      <div className={className} key={key}>
+        { row.map((cell, j) => this.renderPieceCell(cell, i, j)) }
+      </div>
+    )
+  }
+
+  renderPieceCell(cell, i, j) {
+    const key       = `piece-cell-${i}-${j}`
+    let className = `piece-cell ${key}`
+
+    if(cell !== ' ') {
+      className = `${className} piece-cell-full`
+    }
+
+    return (
+      <div className={className} key={key} style={{ backgroundColor: Pieces.COLORS[cell] }}>
+        &nbsp;
+      </div>
+    )
+  }
+}
+
+export default NextPieces;
